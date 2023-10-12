2c695231d8d596cbfb7ebbbb5f32ede931281b49
fahadtufail007 <fahadtufail873@gmail.com>
2023-10-12 12:02:43 +0500

    small fix

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -40,7 +40,7 @@ const Category = () => {
           </div>
         </div>
         <div className="">
-          <div className="grid grid-cols-6 gap-2 border-b-[1px] border-black pb-5">
+          <div className="grid grid-cols-6 gap-2 border-b-[1px] border-black pb-5" id="categoryFadeTextBorder">
             <div className="col-span-2">
               <div className="flex items-center gap-2 ml-4 mt-2">
                 <Button
@@ -60,6 +60,7 @@ const Category = () => {
             <div className="col-span-4">
               <div
                 className={`${style.categoryHeading} col-span-4 text-[#00000080] mt-[4vh] sm:mt-[3vh]`}
+                id="categoryFadeText"
               >
                 <h1>Inquisitive</h1>
                 <h1>Critical • Profound</h1>
@@ -70,7 +71,7 @@ const Category = () => {
 
         <div className="grid grid-cols-6 gap-2">
           <div className="col-span-2"></div>
-          <div className="col-span-4">
+          <div className="col-span-4" id="categoryFadeText">
             <div className="mt-[20px] sm:mt-[30px] md:mt-[40px] lg:mt-[70px] xl:mt-[80px] 2xl:mt-[80px]">
               <div className={`${style.categorySubHeading} text-[#00000080]`}>
                 <div>Peripatetic Refers</div>
@@ -149,7 +150,7 @@ const Category = () => {
       </div>
       </div>
 
-      <div className="categoriesTransition absolute top-1/2 right-0 translate-x-[84.2%] -translate-y-[35%] w-full">
+      <div className="absolute top-1/2 right-0 translate-x-[84.2%] -translate-y-[35%] w-full" id="categoriesTransition">
         <CategoriesCardMain/>
       </div>
     </div>
